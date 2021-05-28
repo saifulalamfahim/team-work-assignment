e25ad68a8d3c39815f961ff7497da2f72554c773
saifulalamfahim <fahim12345saiful@gmail.comm>
2021-05-28 16:33:49 +0600

    last

---
+++ src/Components/Deshborad/Deshborad.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Deshborad = () => {
+    return (
+        <div>
+            <h1>This is Deshboard</h1>
+        </div>
+    );
+};
+
+export default Deshborad;
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>This is Team Work Assignment</h1>
+        </div>
+    );
+};
+
+export default Home;
